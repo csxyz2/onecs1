e3aeb101a55581276a43d4c672d10b04ceb08bee
csxyz2 <140935187+csxyz2@users.noreply.github.com>
2023-07-30 18:53:18 +0530

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -17,7 +17,7 @@ module.exports = {
   kvPrefix: process.env.KV_PREFIX || '',
 
   // The name of your website. Present alongside your icon.
-  title: "Cines-Cloudz",
+  title: "2Cines-Cloudz",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/',
